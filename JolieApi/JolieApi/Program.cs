bd60f62709d39bbf3463b71c3671bccd3e2fe6b9
thanat-base <thanat.sun19821@gmail.com>
2022-11-13 01:40:33 +0700

    migrate database when app start

--- JolieApi/JolieApi/Program.cs
+++ JolieApi/JolieApi/Program.cs
@@ -89,5 +89,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<JolieDataContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.Run();
 
